8993f623b3e1fe284751c493d4691cde274c3e9b
Manuel <mussomanuu@gmail.com>
2021-06-03 17:11:07 +0200

    piccole modifiche grafica

--- component.js
+++ component.js
@@ -14,60 +14,69 @@ app.component("item-detail", {
   template:
     /*html*/
     `
-<div class='container d-flex justify-content-center'>
+    <div class='container d-flex justify-content-center'>
     <div class="card" style="width: 40rem;">
-      <img :src='"https://image.tmdb.org/t/p/w500"+tvDetail.backdrop_path' style="width=50%" class="card-img-top" alt="...">
-    </div>  
-  </div>
-  <h5 class="card-title">{{tvDetail.name}}</h5>
-    <p class="card-text">{{tvDetail.overview}}</p>        
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">
-    <span class="badge bg-primary ms-1" v-for="genre in tvDetail.genres">
-    {{genre.name}}
-    </span>
-    </li>
-    <li class="list-group-item">
-     Data di rilascio: {{tvDetail.first_air_date}}
-    
-    </li>
-    <li class="list-group-item">
-     Stagioni: {{tvDetail.number_of_seasons}}
-    </li>
-    <li class="list-group-item">
-     Episodi: {{tvDetail.number_of_episodes}}
-    </li>
-    <li class="list-group-item">
-    Ultimo rilascio: {{tvDetail.last_air_date}}
-    </li>
-    <li class="list-group-item">
-      <rating :rating="tvDetail.vote_average" />
-    </li>
-    <li class="list-group-item">
-    <span class="ms-1" v-for="companies in tvDetail.production_companies">
-    <br/>
-    {{companies.name}}
-    </span>
-    </li>
-    <li class="list-group-item" v-if="tvDetail.homepage">
+        <img :src='"https://image.tmdb.org/t/p/w500"+tvDetail.backdrop_path' style="width=50%" class="card-img-top"
+            alt="...">
+        <h5 class="card-title">{{tvDetail.name}}</h5>
+        <p class="card-text">{{tvDetail.overview}}</p>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <span class="badge bg-primary ms-1" v-for="genre in tvDetail.genres">
+                    {{genre.name}}
+                </span>
+            </li>
+            <li class="list-group-item">
+                Data di rilascio: {{tvDetail.first_air_date}}
 
-    <a :href="tvDetail.homepage" target="blank" class="card-link">Link homepage ({{ getHomepage(tvDetail.homepage) }})</a>
+            </li>
+            <li class="list-group-item">
+                Stagioni: {{tvDetail.number_of_seasons}}
+            </li>
+            <li class="list-group-item">
+                Episodi: {{tvDetail.number_of_episodes}}
+            </li>
+            <li class="list-group-item">
+                Ultimo rilascio: {{tvDetail.last_air_date}}
+            </li>
+            <li class="list-group-item">
+                <rating :rating="tvDetail.vote_average" />
+            </li>
+            <li class="list-group-item">
+                <span class="ms-1" v-for="companies in tvDetail.production_companies">
+                    <br />
+                    {{companies.name}}
+                </span>
+            </li>
+            <li class="list-group-item" v-if="tvDetail.homepage">
 
-    </li>
-  </ul>
-  <div class="card-body" v-if="length">
-    <h3 v-if="tvDetail.created_by.length">Creatori </h3>
-    <div class="card-group" v-if="tvDetail.created_by.length">
-      <div class="card" v-for="creator in tvDetail.created_by">
-        <img  v-if="creator.profile_path" :src='"https://image.tmdb.org/t/p/original"+creator.profile_path' class="rounded mx-auto d-block"  style="max-width: 15rem; " class="card-img-top " alt="...">
+                <a :href="tvDetail.homepage" target="blank" class="card-link">Link homepage ({{
+                    getHomepage(tvDetail.homepage)
+                    }})</a>
+
+            </li>
+        </ul>
         <div class="card-body">
-            <div class="card text-center">
-              <h5> {{creator.name}} </h5>
+            <h3 v-if="tvDetail.created_by.length">Creatori </h3>
+
+            <div class="card-group" v-if="tvDetail.created_by.length">
+                <div class="card" v-for="creator in tvDetail.created_by">
+                    <img v-if="creator.profile_path" :src='"https://image.tmdb.org/t/p/original"+creator.profile_path'
+                        class="rounded mx-auto d-block" style="max-width: 15rem; " class="card-img-top " alt="...">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <h5> {{creator.name}} </h5>
+
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <a :href="'./person.html?id='+creator.id" class="btn btn-primary">More details</a>
+                    </div>
+                </div>
             </div>
-          </div>  
-      </div>
+        </div>
     </div>
-  </div>
+</div>
   `,
   data() {
     return {
--- persondetails.js
+++ persondetails.js
@@ -14,11 +14,23 @@ app.component("person-detail", {
     template:
         /*html*/
         `
-<div class="card text-center">
-  <div class="card" v-for="person in tvDetail.created_by">
-  <img :src='"https://image.tmdb.org/t/p/w500"+person.profile_path'  style="width: 18rem;" class="card-img-top" alt="...">
+<div class="d-flex justify-center">
+<div class="card m-auto" style="width: 25rem;">
+  <img :src='"https://image.tmdb.org/t/p/w500"+person.profile_path' class="card-img-top" alt="...">
   <div class="card-body">
+    <h5 class="card-title">{{person.name}}
+    <i class="fas fa-mars text-primary" v-if="person.gender==2"></i>
+    
+    <i v-else class="fas fa-venus text-danger"></i>
+    </p>
+    </h5>
     <p class="card-text"></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Data di nascita: {{person.birthday}}</li>
+    <li class="list-group-item">Luogo di nascita: {{person.place_of_birth}}</li>
+  </ul>
+  <div class="card-body">
   </div>
 </div>
 </div>
